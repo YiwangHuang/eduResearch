3f3bd0d19f33aed6090351d7b727afc4b4339fe7
yiwanghuang <yiwanghuang97@gmail.com>
2025-05-02 21:38:45 +0800

    init from plugins-for-anyBlock

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,19 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { markdownAnyblockPlugin } from 'vuepress-plugin-markdown-anyblock'
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "VuePress 博客",
+  description: "vuepress-theme-hope 的博客演示",
+  theme,
+  // extendsMarkdown: (md) => {
+  //   md.use(ab_mdit)
+  // },
+  plugins: [
+    markdownAnyblockPlugin(),
+  ],
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
